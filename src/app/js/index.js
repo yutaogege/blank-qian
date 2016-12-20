71fd6c80bd898f1756c35063f8570d87f694f178
Jeeeeeeean <599175847@qq.com>
2016-12-20 10:02:03 +0800

    1220补充提交
    1220补充提交

--- src/app/js/index.js
+++ src/app/js/index.js
@@ -1,10 +1,32 @@
 $(function() {
+    // alert(wWidth = $(window).width());
     // 判断是否为ios系统，决定是否让视频自动播放
     var UA = navigator.userAgent;
     var ios = !!UA.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
     if (!ios) {
         // $('.video')[0].play();
     }
+
+    var hideTimer = null ;
+    $(function() {
+        $(".J_n_hover").hover(function() {
+            clearTimeout(hideTimer); //清除计时器
+            $(this).addClass('nHover');
+        }, function() {
+            var $that = $(this);
+            hideTimer = setTimeout(function(){
+                $that.removeClass('nHover');
+            }, 500);
+        });
+    })
+    // var hideTimer;
+    // $(function() {
+    //     $(".nHover").mouseleave(function() {
+    //         console.log('test');
+    //         clearTimeout(hideTimer); //清除计时器
+    //         hideTimer = setTimeout("$('.mouseleave').hide();", 1000); //鼠标移除元素区域子元素消失
+    //     });
+    // })
 
     $('.J_nav_btn').click(function() {
         var nav_min = $('.nav-min-lv1');
@@ -23,11 +45,15 @@ $(function() {
             _banner = $('.banner'),
             wHeight = $(window).height(),
             wWidth = $(window).width(),
+            reHeightB = wWidth / 1080 * 1920 + 'px',
             reHeight = wHeight - $('.header-wrap').height() + 'px',
             vHeight = wWidth / 1920 * 1080 + 'px',
             wRate = parseInt(wWidth) / parseInt(reHeight) > 1920 / 1080;
-
-        _banner.height(reHeight);
+        if ($(window).width()>610) {
+            _banner.height(reHeight);
+        } else {
+            _banner.height(reHeightB);
+        }
 
         if (wWidth < 610) {
             _videoBox.height(vHeight);
@@ -52,29 +78,28 @@ $(function() {
     });
 
     // 在小屏幕下让导航被点击后收起
-     var navMin= $('.nav-min-lv1');
-     navMin.click(function(event) {
-         if (navMin.hasClass('open')) {
+    var navMin = $('.nav-min-lv1');
+    navMin.click(function(event) {
+        if (navMin.hasClass('open')) {
             navMin.removeClass('open');
             $('.min-nav-btn').removeClass('open')
-         }
-     });
+        }
+    });
 
-     // 视频控制
-    $('video').on("canplay", function(){
+    // 视频控制
+    $('video').on("canplay", function() {
         $('.video-mask').css('display', 'none');
-      }
-    );
+    });
 
-     // 监听滚动条并且嘿嘿嘿
-     if (!ios) {
-        $(window).on('scroll',function(event) {
+    // 监听滚动条并且嘿嘿嘿
+    if ($(window).width()>610) {
+        $(window).on('scroll', function(event) {
             if ($('.video').hasClass('ed')) {
                 return;
             }
-            var wHeight = $(window).height()*0.7,
+            var wHeight = $(window).height() * 0.7,
                 sHeight = $(this).scrollTop();
-            if (wHeight<sHeight) {
+            if (wHeight < sHeight) {
                 // console.log(sHeight);
                 $('.video')[0].play();
             }
@@ -82,19 +107,17 @@ $(function() {
     }
 
     // 播放ing
-    $('video').on("play", function(){
+    $('video').on("play", function() {
         $('.play').css('display', 'none');
         $('.video-mask').css('display', 'none');
-      }
-    );
+    });
 
     // 播完操作
-    $('video').on("ended", function(){
+    $('video').on("ended", function() {
         $('.video').addClass('ed');
         $('.video-mask').css('display', 'block');
         $('.play').css('display', 'block');
-      }
-    );
+    });
 
     // 视频再次播放
     $('.play').click(function() {
